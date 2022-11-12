38ff8d1ea40a4a714cad7394ff15cc5af192ec6b
rumiyoshikuni <115619656+rumiyoshikuni@users.noreply.github.com>
2022-11-12 11:26:13 +0000

    [Finish]20~24

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,38 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
-    @user = User.find(params[:id])
+  user_id = params[:id].to_i
+  login_user_id = current_user.id
+  if(user_id != login_user_id)
+    redirect_to post_images_path
   end
+
+  @user = User.find(params[:id])
+end
+  
+def update
+
+  user_id = params[:id].to_i
+  login_user_id = current_user.id
+  if(user_id != login_user_id)
+    redirect_to post_images_path
+  end
+
+  @user = User.find(params[:id])
+  @user.update(user_params)
+  redirect_to user_path(@user.id)
+end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 end
