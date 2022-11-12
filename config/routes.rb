976291f698e6e25a569a0b1afe06325be99469e5
rumiyoshikuni <115619656+rumiyoshikuni@users.noreply.github.com>
2022-11-12 00:03:34 +0000

    [Add]16~19

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  get "/homes/about", to: "homes#about", as: 'about'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  get "/homes/about" => "homes#about", as: "about"
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create]
+  end
+  resources :users, only: [:show, :edit, :update]
 end
